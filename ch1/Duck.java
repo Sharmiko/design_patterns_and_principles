7452d4737cdd3b59da6f280ca05f2380687b85e6
Sharmiko <sharmiko123@gmail.com>
2020-12-09 00:42:12 +0400

    update first chapter

--- ch1/Duck.java
+++ ch1/Duck.java
@@ -3,6 +3,14 @@ public abstract class Duck {
     FlyBehavior flyBehavior;
     QuackBehavior quackBehavior;
 
+    public void setFlyBehavior(FlyBehavior fb) {
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb) {
+        quackBehavior = qb;
+    }
+
     public abstract void display();
 
     public void performFly() {
